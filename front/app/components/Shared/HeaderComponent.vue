526a147e1a230e40120cd7bc07c840640b981b1c
Lekvar <145697822+Lekvar312@users.noreply.github.com>
2025-12-12 23:39:57 +0200

    create nuxt app

---
+++ front/app/components/Shared/HeaderComponent.vue
@@ -0,0 +1,8 @@
+<script setup></script>
+<template>
+  <header class="flex items-center">
+    <div>
+      <span class="bg-blue text-white">УЖ</span>
+    </div>
+  </header>
+</template>
---
+++ front/app/layouts/default.vue
@@ -0,0 +1,9 @@
+<script setup></script>
+<template>
+  <div class="w-full max-w-[1500px] m-auto">
+    <HeaderComponent />
+    <main>
+      <NuxtPage />
+    </main>
+  </div>
+</template>
