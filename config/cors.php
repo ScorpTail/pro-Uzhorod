66e8a42ec0bf0b1e86e14d190103a19959e342cc
Danchevskyi Vladyslav <scorpio232354@gmail.com>
2025-12-18 22:29:12 +0200

    feat: update CORS configuration to include CSRF cookie path and refine allowed methods and headers

--- config/cors.php
+++ config/cors.php
@@ -15,20 +15,19 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'paths'                    => ['api/*'],
 
-    'allowed_methods' => ['*'],
+    'allowed_methods'          => ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins'          => ['*'],
 
     'allowed_origins_patterns' => [],
 
-    'allowed_headers' => ['*'],
+    'allowed_headers'          => ['Content-Type', 'Authorization', 'X-Requested-With'],
 
-    'exposed_headers' => [],
+    'exposed_headers'          => [],
 
-    'max_age' => 0,
+    'max_age'                  => 0,
 
-    'supports_credentials' => false,
-
+    'supports_credentials'     => true,
 ];
